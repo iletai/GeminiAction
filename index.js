9d83f1b83985aaf0632e87cb29850bf980ed5f44
Tài Lê <iletai@hotmail.com>
2024-04-05 10:24:49 +0700

    + Add gemini input example (#5)

--- index.js
+++ index.js
@@ -2,46 +2,108 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { GenerativeModel } from "@google/generative-ai";
 
-async function run() {
-    const apiKey = core.getInput("gemini-api-key");
-    const githubToken = core.getInput("github-token");
-    const octokit = github.getOctokit(githubToken);
-    const prTemplate = await github.context.repo;
-    const model = new GenerativeModel(apiKey);
-    const issue = await octokit.rest.issues.get({
-        ...github.context.issue,
-        issue_number: github.context.issue.number,
-      });
-    const availableLabels = await octokit.rest.issues.listLabelsForRepo({
-    ...github.context.repo,
-    }); 
-    const availablePullRequests = await octokit.rest.pulls.list({
-        ...github.context.repo,
-    });
-    // Get First Pull Request If Exist. Avoid Null
-    if (availablePullRequests.data.length === 0) {
-        console.log("No Pull Requests Available");
-        return;
-    }
-    const firstPullRequest = availablePullRequests.data[0];
-    const pullRequest = await octokit.rest.pulls.get({
-        ...github.context.repo,
-        pull_number: firstPullRequest.number,
-    });
-    // Comment Pull Request
-    await octokit.rest.pulls.createReview({
-        ...github.context.repo,
-        pull_number: pullRequest.data.number,
-        body: "Hello from Gemini!",
-    });
-    // await octokit.rest.issues.createComment({
-    //     ...github.context.repo,
-    //     issue_number: pullRequest.data.number,
-    //     body: "Hello from Gemini!",
-    // });
-    console.log(prTemplate);
-    console.log(availableLabels);
-    console.log(issue); 
-}
+// Define the valid parity labels.
+// Get Pull Request Number From Ref Revision
+const getPullRequestNumber = (ref) => {
+    core.debug(`[GeminiAction]  Parsing ref: ${ref}`);
+    // This assumes that the ref is in the form of `refs/pull/:prNumber/merge`
+    const prNumber = ref.replace(/refs\/pull\/(\d+)\/merge/, `$1`);
+    return parseInt(prNumber, 10);
+};
 
-run();
+(async () => {
+    try {
+        const owner = github.context.repo.owner;
+        const repo = github.context.repo.repo;
+        const apiKey = core.getInput("gemini-api-key");
+        const githubToken = core.getInput("github-token");
+        const ref = github.context.ref;
+        const octokit = github.getOctokit(githubToken);
+        const prNumber = github.context.issue.number || getPullRequestNumber(ref);
+        const model = new GenerativeModel(apiKey, {
+            model: "gemini-pro",
+            generationConfig: { temperature: 0 },
+        });
+
+        core.debug({ completion });
+
+        let labels = /LABELS\: (.+)/g.exec(completion.response.text());
+
+        if (labels) {
+            labels = labels[1].trim().split(/,\s*/);
+
+            await octokit.rest.issues.setLabels({
+                owner: github.context.issue.owner,
+                repo: github.context.issue.repo,
+                issue_number: github.context.issue.number,
+                labels,
+            });
+        } else {
+            core.setFailed(
+                `Failed to propose labels: completion=${completion.data.choices[0].text}`,
+            );
+        }
+        const availableIssuesLabels = async (issuesNumber) => {
+            const { data } = await octokit.issues.listLabelsOnIssue({
+                issue_number: issuesNumber,
+                owner,
+                repo,
+            });
+            if (data.length === 0) {
+                throw new Error(`[GeminiAction]  No Issues found for ${issuesNumber} (${ref}).`);
+            }
+            return data.labels.map((label) => label.name);
+        };
+        const issue = await octokit.rest.issues.get({
+            ...github.context.issue,
+            issue_number: github.context.issue.number,
+        });
+
+        const availableLabels = await octokit.rest.issues.listLabelsForRepo({
+            ...github.context.repo,
+        });
+        const getPrLabels = async (prNumber) => {
+            const { data } = await octokit.pulls.get({
+                pull_number: prNumber,
+                owner,
+                repo,
+            });
+            if (data.length === 0) {
+                throw new Error(`[GeminiAction]  No Pull Requests found for ${prNumber} (${ref}).`);
+            }
+            return data.labels.map((label) => label.name);
+        };
+
+        const prLabels = await getPrLabels(prNumber);
+        core.debug(`[GeminiAction] Found PR labels: ${prLabels.toString()}`);
+        // Get the valid parity labels in this pull request.
+        const prValidLabels = prLabels.filter(value => validLabels.includes(value));
+        const prompt = `
+        You have a role to manage a GitHub repository. Given an issue/pull request information (subject and body), choose suitable labels to it from the labels available for the repository.
+    
+        Use the following format:
+        LABELS: "the names of the chosen labels, each name must not be surrounded double quotes, separated by a comma"
+    
+        Only use the following labels:
+        \`\`\`
+        ${JSON.stringify(availableLabels.data, null, 2)}
+        \`\`\`
+    
+        ## ISSUE/PULL REQUEST ##
+        SUBJECT: ${issue.data.title} or ${prLabels.data.title}
+        BODY: ${issue.data.body} or ${prLabels.data.body}
+      `;
+        core.debug(`Prompt: ${prompt}`);
+        if (prValidLabels.length > 0) {
+            core.info(`OK: Pull Request has at least one parity label.`);
+        }
+        else {
+            core.error(`Missing parity label: The PR should have at least one of these labels: ${validLabels.join(`, `)}`);
+            throw `[GeminiAction] No labels exist in the PR. Please add at least one of these labels: ${validLabels.join(`, `)}`;
+        }
+        const completion = await model.generateContent(prompt);
+        core.debug({ completion });
+    } catch (error) {
+        core.setFailed(`Error Message: ${error.stack}`);
+    }
+})();
